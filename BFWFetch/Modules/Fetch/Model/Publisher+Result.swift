2db8797cd7e9f201230693cf87a00ec2d4c15879
Tom Brodhurst-Hill <developer@barefeetware.com>
2021-04-14 15:01:17 +1000

    Publisher+Result: for any Publisher, not AnyPublisher

--- BFWFetch/Modules/Fetch/Model/Publisher+Result.swift
+++ BFWFetch/Modules/Fetch/Model/Publisher+Result.swift
@@ -1,5 +1,5 @@
 //
-//  AnyPublisher+Result.swift
+//  Publisher+Result.swift
 //  BFWFetch
 //
 //  Created by Tom Brodhurst-Hill on 13/4/21.
@@ -9,7 +9,7 @@
 import Foundation
 import Combine
 
-public extension AnyPublisher {
+public extension Publisher {
     
     /// Map into a result publisher, which never fails. The Failure moves into the result.
     func result() -> AnyPublisher<Result<Output, Failure>, Never> {
