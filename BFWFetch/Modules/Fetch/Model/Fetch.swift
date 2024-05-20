f26fa414e902ccaffd221bf0a85493718bc8bf58
Tom Brodhurst-Hill <developer@barefeetware.com>
2024-05-20 17:40:30 +1000

    Fetch.Header: public init.

--- BFWFetch/Modules/Fetch/Model/Fetch.swift
+++ BFWFetch/Modules/Fetch/Model/Fetch.swift
@@ -51,8 +51,14 @@ public extension Fetch {
     }
     
     struct Header {
+        
         let key: String
         let value: String
+        
+        public init(key: String, value: String) {
+            self.key = key
+            self.value = value
+        }
         
         var dictionary: [String: String] { [key: value] }
         
