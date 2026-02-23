5f6a2b204677859380f929d90c2afd7fab72b060
Tom Brodhurst-Hill <developer@barefeetware.com>
2026-02-23 13:08:45 +1100

    OAuth.Credential: expires_in fallback to string if not numeric

--- BFWFetch/Modules/Fetch/Model/OAuth.swift
+++ BFWFetch/Modules/Fetch/Model/OAuth.swift
@@ -12,22 +12,13 @@ public enum OAuth {}
 // MARK: - Types
 
 extension OAuth {
-    
-    public struct Credential: Decodable {
+
+    public struct Credential {
         public let accessToken: String?
         public let tokenType: String?
         public let expiresTimeInterval: TimeInterval?
         public var refreshToken: String?
         public let idToken: String?
-        
-        enum CodingKeys: String, CodingKey {
-            case accessToken = "access_token"
-            case tokenType = "token_type"
-            case expiresTimeInterval = "expires_in"
-            case refreshToken = "refresh_token"
-            case idToken = "id_token"
-        }
-        
     }
     
     public enum Grant: String {
@@ -35,6 +26,36 @@ extension OAuth {
         case refreshToken = "refresh_token"
         /// For partner app registration?
         case clientCredentials = "client_credentials"
+    }
+    
+}
+
+// MARK: - Protocol Conformances
+
+extension OAuth.Credential: Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case tokenType = "token_type"
+        case expiresTimeInterval = "expires_in"
+        case refreshToken = "refresh_token"
+        case idToken = "id_token"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        accessToken = try container.decodeIfPresent(String.self, forKey: .accessToken)
+        tokenType = try container.decodeIfPresent(String.self, forKey: .tokenType)
+        refreshToken = try container.decodeIfPresent(String.self, forKey: .refreshToken)
+        idToken = try container.decodeIfPresent(String.self, forKey: .idToken)
+        // RFC 6749 specifies expires_in as a number, but some servers return a string.
+        if let value = try? container.decodeIfPresent(TimeInterval.self, forKey: .expiresTimeInterval) {
+            expiresTimeInterval = value
+        } else if let string = try? container.decodeIfPresent(String.self, forKey: .expiresTimeInterval) {
+            expiresTimeInterval = TimeInterval(string)
+        } else {
+            expiresTimeInterval = nil
+        }
     }
     
 }
