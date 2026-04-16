2b9f0e03ccfc28b7bee251c2e4065fa8fe193910
Tom Brodhurst-Hill <developer@barefeetware.com>
2026-04-16 15:17:57 +1000

    OpenEnum

---
+++ BFWFetch/Modules/Fetch/Model/OpenEnum.swift
@@ -0,0 +1,59 @@
+//
+//  OpenEnum.swift
+//  BFWFetch
+//
+//  Created by Tom Brodhurst-Hill on 25/8/2025.
+//  Copyright © 2025 BareFeetWare. All rights reserved.
+//
+
+import Foundation
+
+/// Wrapper for a String RawRepresentable enum, tolerating unknown values that aren't in the Known type.
+public enum OpenEnum<Known: RawRepresentable & Sendable> where Known.RawValue == String {
+    case known(Known)
+    case unknown(String)
+}
+
+extension OpenEnum: Decodable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let stringValue = try container.decode(String.self)
+        if let value = Known(rawValue: stringValue) {
+            self = .known(value)
+        } else {
+            self = .unknown(stringValue)
+        }
+    }
+}
+
+extension OpenEnum: Encodable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .known(let value):
+            try container.encode(value.rawValue)
+        case .unknown(let stringValue):
+            try container.encode(stringValue)
+        }
+    }
+}
+
+extension OpenEnum: Equatable where Known: Equatable {}
+
+extension OpenEnum: RawRepresentable {
+
+    public init?(rawValue: String) {
+        if let known = Known(rawValue: rawValue) {
+            self = .known(known)
+        } else {
+            self = .unknown(rawValue)
+        }
+    }
+
+    public var rawValue: String {
+        switch self {
+        case .known(let known): known.rawValue
+        case .unknown(let unknown): unknown
+        }
+    }
+}
